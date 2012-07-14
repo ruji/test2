3e824110e689386072a95e9ba846a0e47f145605
John Smith <luzi@pilot.trilug.org>
2012-07-14 10:06:17 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+T2::Application.config.secret_token = 'b54b1cd7c0e2b4cecfeb484c54de233c47eb3b4217f610c658c29bc47b5e6f364a92d8d0159ce37bca7f585a3896d38220fe855b70a3e045576cf18c623f20b4'
